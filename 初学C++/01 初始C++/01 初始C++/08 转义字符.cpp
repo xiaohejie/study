a63b9d4d602b63ac1cb3939a975c6c5576c03268
ajie <2234842317@qq.com>
2021-10-25 19:34:34 +0800

    学习C++

---
+++ 初学C++/01 初始C++/01 初始C++/08 转义字符.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main8() {
+	
+	cout << "bcuewygre\thello" << endl;
+	cout << "bcuewyg\thello" << endl;
+	cout << "bcuee\thello" << endl;
+	system("pause");
+	return 0;
+}
---
+++ 初学C++/01 初始C++/01 初始C++/09 字符串型.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main9() {
+	//1、C风格的字符串
+	char str[] = "hello world";
+	cout << str << endl;
+	//2、C++风格的字符串
+	string str2 = "hello hejie";
+	cout << str2 << endl;
+	system("pause");
+	return 0;
+}
