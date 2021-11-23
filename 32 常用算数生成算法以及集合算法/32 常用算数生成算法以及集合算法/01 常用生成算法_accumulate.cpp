b799ed9746b3c5df69342ce8dcd65b8977065a15
ajie <2234842317@qq.com>
2021-11-23 23:14:40 +0800

    常用算数生成算法以及集合算法

---
+++ 32 常用算数生成算法以及集合算法/32 常用算数生成算法以及集合算法/01 常用生成算法_accumulate.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include <vector>
+#include <numeric>
+using namespace std;
+/*
+	`accumulate(iterator beg, iterator end, value);  `
+		// 计算容器元素累计总和
+		// beg 开始迭代器
+		// end 结束迭代器
+		// value 起始值
+*/
+void test01() {
+	vector<int>v;
+	for (int i = 0; i <= 100; i++)
+	{
+		v.push_back(i);
+	}
+	int total = accumulate(v.begin(), v.end(), 0);
+	cout << total << endl;
+}
+int main01() {
+	test01();
+	system("pause");
+	return 0;
+}
---
+++ 32 常用算数生成算法以及集合算法/32 常用算数生成算法以及集合算法/02 常用生成算法_fill.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+/*
+	`fill(iterator beg, iterator end, value);  `
+		// 向容器中填充元素
+		// beg 开始迭代器
+		// end 结束迭代器
+		// value 填充的值
+*/
+class myPrint
+{
+public:
+	void operator()(int val)
+	{
+		cout << val << " ";
+	}
+};
+
+void test02()
+{
+
+	vector<int> v;
+	v.resize(10);
+	//填充
+	fill(v.begin(), v.end(), 100);
+
+	for_each(v.begin(), v.end(), myPrint());
+	cout << endl;
+}
+
+int main02() {
+
+	test02();
+
+	system("pause");
+
+	return 0;
+}
